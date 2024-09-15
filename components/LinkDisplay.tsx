3e523652f1d42fc09806c6b5cbd0ef91fdaf6dc2
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-09-15 04:58:54 -0400

    link display fix

--- components/LinkDisplay.tsx
+++ components/LinkDisplay.tsx
@@ -36,50 +36,49 @@ export default function LinkDisplay({ linkData }: LinkDisplayProps) {
   const MediaTypeIcon = mediaTypeIcons[linkData.mediaType];
 
   return (
-    <Card className="w-full max-w-md bg-black border-gray-800 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl hover:border-gray-400 fade">
-      <CardHeader className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
-        <div className="flex items-center space-x-2">
-          <div className="p-2 rounded">
-            <MediaTypeIcon className="h-6 w-6 text-gray-400" />
+    <div className="w-full flex flex-col justify-between max-w-md bg-black border rounded-lg border-gray-800 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl hover:border-gray-400 fade">
+
+
+      <div className="w-[90%] mx-auto">
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center my-4">
+              <MediaTypeIcon className="h-4 w-4 text-gray-400" />
+            <span className="text-gray-400 ml-2 text-xs sm:text-base">
+              {linkData.mediaType.charAt(0).toUpperCase() + linkData.mediaType.slice(1).toLowerCase()}
+            </span>
           </div>
-          <span className="text-gray-400 text-sm sm:text-base">
-            {linkData.mediaType.charAt(0).toUpperCase() + linkData.mediaType.slice(1).toLowerCase()}
+          <span className="text-gray-400 text-xs mr-2">
+            {format(new Date(linkData.date), "MM/dd/yyyy")}
           </span>
+          
         </div>
-        <span className="text-gray-400 text-sm sm:text-base">
-          {format(new Date(linkData.date), "MM/dd/yyyy")}
-        </span>
-      </CardHeader>
-      <CardContent className="space-y-2">
+
         <a
           target="_blank"
           href={linkData.url}
-          className="text-base sm:text-lg md:text-xl font-bold text-white"
+          className="text-base sm:text-md font-bold text-white"
         >
           {linkData.title}
         </a>
-        <p className="text-gray-400 text-xs sm:text-sm md:text-base">
+        <p className="text-gray-400 text-sm mt-4">
           {linkData.description}
         </p>
-      </CardContent>
-      <CardFooter className="flex flex-col items-stretch">
-        <div className="space-x-2 mb-2 sm:mb-0">
+      </div>
+
+      <CardFooter className="flex items-center justify-between mt-6">
+        <div className="">
           {linkData.tags.map((tag, index) => (
             <span
               key={index}
-              className="text-gray-200 text-xs sm:text-sm py-1 px-2 rounded-full bg-gray-800"
+              className="text-gray-200 text-xs sm:text-sm py-1 px-2 mx-1 rounded-full bg-gray-800"
             >
               #{tag}
             </span>
           ))}
         </div>
-        <div className="flex justify-between items-center w-full mt-8">
-          <Link
-            className="text-gray-400 text-xs sm:text-sm md:text-base hover:text-purple-600"
-            href={`/profile/${linkData.userID}`}
-          >
-            {/* Posted by {linkData.profile.username} */}
-          </Link>
+
+        <div className="">
           <LinkPreview
             url={linkData.url}
             className="flex items-center text-smrounded cursor-pointer text-pink-500 opacity-40 hover:opacity-100"
@@ -89,6 +88,6 @@ export default function LinkDisplay({ linkData }: LinkDisplayProps) {
           </LinkPreview>
         </div>
       </CardFooter>
-    </Card>
+    </div>
   );
 }
